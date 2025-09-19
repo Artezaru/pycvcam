736bba471df1ac7c8d5e9bab7a1a33931aa2cbd9
Artezaru <artezaru.github@proton.me>
2025-09-19 13:27:20 +0200

    Default-commit-message

--- pycvcam/optimize/optimize_parameters.py
+++ pycvcam/optimize/optimize_parameters.py
@@ -21,6 +21,10 @@ from numbers import Number
 from ..core.transform import Transform
 from ..core.package import Package
 
+def log_verbose(verbose_level: int, criterion: int, message: str):
+    if verbose_level >= criterion:
+        print(message)
+
 def optimize_parameters(
     transform: Transform,
     input_points: numpy.ndarray,
@@ -29,14 +33,15 @@ def optimize_parameters(
     *,
     transpose: bool = False,
     max_iter: int = 10,
+    max_time: Optional[float] = None,
     delta_p_threshold: Optional[float] = None,
     eps_threshold: Optional[float] = None,
+    eps_sum_threshold: Optional[float] = None,
     gradient_threshold: Optional[float] = None,
-    max_time: Optional[float] = None,
     cond_cutoff: Optional[float] = None,
     reg_factor: Optional[float] = None,
     precond_jacobi: bool = False,
-    verbose: bool = False,
+    verbose_level: int = 0,
     _skip: bool = False,
     ) -> numpy.ndarray:
     r"""
@@ -111,6 +116,9 @@ def optimize_parameters(
 
     max_iter : int, optional
         The maximum number of iterations for the optimization. Default is 10. The optimization stops if the maximum number of iterations is reached.
+    
+    max_time : Optional[float], optional
+        If given, the optimization stops if the elapsed time is greater than `max_time` seconds. Default is None, which means no time limit is applied.
 
     delta_p_threshold : Optional[float], optional
         If given, the optimization compute :math:`\|\delta \lambda\|` at each iteration and stops if :math:`\|\delta \lambda\| < \text{delta_p_threshold}`. Default is None, which means no threshold is applied.
@@ -119,11 +127,11 @@ def optimize_parameters(
         If given, the optimization compute the mean of :math:`\|\vec{X}_O - T(\vec{X}_I, \lambda)\|` at each iteration. All points with a difference lower than `eps_threshold` are considered converged.
         The optimization stops if all points are converged. Default is None, which means no threshold is applied.
 
+    eps_sum_threshold : Optional[float], optional
+        If given, the optimization compute the sum of :math:`\|\vec{X}_O - T(\vec{X}_I, \lambda)\|` at each iteration and stops if :math:`\sum \|\vec{X}_O - T(\vec{X}_I, \lambda)\| < \text{eps_sum_threshold}`. Default is None, which means no threshold is applied.
+
     gradient_threshold : Optional[float], optional
         If given, the optimization compute :math:`\|J^{T} R\|` at each iteration and stops if :math:`\|J^{T} R\| < \text{gradient_threshold}`. Default is None, which means no threshold is applied.
-
-    max_time : Optional[float], optional
-        If given, the optimization stops if the elapsed time is greater than `max_time` seconds. Default is None, which means no time limit is applied.
 
     cond_cutoff : Optional[float], optional
         The cutoff value for the condition number of the Jacobian matrix. If the condition number is greater than this value, the optimization will be considered unstable and will raise a warning and return NaN array. This is used to detect ill-conditioned problems. Default is None, which means no cutoff is applied.
@@ -134,8 +142,15 @@ def optimize_parameters(
     precond_jacobi : bool, optional
         If True, apply a preconditioner to the Jacobian matrix to improve the conditioning of the problem. This is done by applying the Jacobi preconditioner to the Jacobian matrix before solving the optimization problem. Default is False.
     
+    verboose_level : int, optional
+        The level of verbosity for the optimization process.
+        - 0: No output (default)
+        - 1: Print warnings, flags reached.
+        - 2: Print iteration result summary (mean, max of residuals and delta parameters norm).
+        - 3: All the above + print condition number and eigenvalues of the Jacobian matrix.    
+
     verbose : bool, optional
-        If True, print the optimization progress and diagnostics. Default is False.
+        If True, print the optimization progress and diagnostics. Default is False. This can be useful for debugging and understanding the optimization process but may slow down the optimization.
 
     _skip : bool, optional
         If True, skip the checks for the transformation parameters and assume the input and output points are given in the (Npoints, input_dim) and (Npoints, output_dim) float format, respectively.
@@ -193,8 +208,8 @@ def optimize_parameters(
             raise TypeError(f"transpose must be a boolean, got {type(transpose)}")
         if not isinstance(max_iter, int) or max_iter <= 0:
             raise TypeError(f"max_iter must be an integer greater than 0, got {max_iter}")
-        if not isinstance(verbose, bool):
-            raise TypeError(f"verbose must be a boolean, got {type(verbose)}")
+        if not isinstance(verbose_level, int) or verbose_level < 0 or verbose_level > 3:
+            raise TypeError(f"verbose_level must be an integer between 0 and 3, got {verbose_level}")
         if cond_cutoff is not None and (not isinstance(cond_cutoff, Number) or cond_cutoff <= 0):
             raise TypeError(f"cond_cutoff must be a positive float, got {cond_cutoff}")
         if reg_factor is not None and (not isinstance(reg_factor, Number) or reg_factor < 0):
@@ -205,6 +220,8 @@ def optimize_parameters(
             raise TypeError(f"max_time must be a positive float, got {max_time}")
         if eps_threshold is not None and (not isinstance(eps_threshold, float) or eps_threshold <= 0):
             raise TypeError(f"eps_threshold must be a positive float, got {eps_threshold}")
+        if eps_sum_threshold is not None and (not isinstance(eps_sum_threshold, float) or eps_sum_threshold <= 0):
+            raise TypeError(f"eps_sum_threshold must be a positive float, got {eps_sum_threshold}")
         if delta_p_threshold is not None and (not isinstance(delta_p_threshold, float) or delta_p_threshold <= 0):
             raise TypeError(f"delta_p_threshold must be a positive float, got {delta_p_threshold}")
         if gradient_threshold is not None and (not isinstance(gradient_threshold, float) or gradient_threshold <= 0):
@@ -263,52 +280,52 @@ def optimize_parameters(
     # Set the parameters of the object class to the guess
     object_class.parameters = guess
     delta_itk = numpy.zeros_like(object_class.parameters, dtype=Package.get_float_dtype())
+    starting_time = time.perf_counter()
+    R_precomputed = None
+    J_precomputed = None
 
+    # ---------------------------------------------
     # Run the iterative algorithm
+    # ---------------------------------------------
     for it in range(max_iter):
-
-        #============================================
-        # Transformation and Jacobian computation
-        #============================================
+        log_verbose(verbose_level, 2, f"\n#=====================================================")
+        log_verbose(verbose_level, 2, f"STARTING ITERATION {it+1} OF THE OPTIMIZATION PROCESS")
+        log_verbose(verbose_level, 2, f"#=====================================================")
 
         # Compute the transformed points and the Jacobian with respect to the parameters
-        transformed_points_itk, _, jacobian_dp = object_class._transform(input_points, dx=False, dp=True)  # shape (Npoints, output_dim), None, (Npoints, output_dim, Nparams)
+        if R_precomputed is not None or J_precomputed is not None:
+            transformed_points_itk, _, jacobian_dp = object_class._transform(input_points, dx=False, dp=True)  # shape (Npoints, output_dim), None, (Npoints, output_dim, Nparams)
 
-        # Check if the jacobian_dp is None$
-        if jacobian_dp is None:
-            raise ValueError("Jacobian with respect to the parameters is not available. Please implement the _transform method to return the Jacobian with respect to the parameters.")
+            # Check if the jacobian_dp is None
+            if jacobian_dp is None:
+                raise ValueError("Jacobian with respect to the parameters is not available. Please implement the _transform method to return the Jacobian with respect to the parameters.")
 
-        # Compute the operator residual
-        R = output_points - transformed_points_itk  # shape (Npoints, output_dim)
-        J = jacobian_dp  # shape (Npoints, output_dim, Nparams)
+            # Compute the operator residual
+            R = output_points - transformed_points_itk  # shape (Npoints, output_dim)
+            J = jacobian_dp  # shape (Npoints, output_dim, Nparams)
 
+        else:
+            R = R_precomputed
+            J = J_precomputed
+        
         # Check the convergence of the optimization
-        if verbose or eps_threshold is not None:
+        if verbose_level >= 2 or eps_threshold is not None or eps_sum_threshold is not None:
             diff = numpy.linalg.norm(R, axis=1)  # shape (Npoints,)
-
-        if verbose:
-            print(f"Iteration {it}: |X_O - X_I| - Max difference: {numpy.nanmax(diff)}, Mean difference: {numpy.nanmean(diff)}")
+            log_verbose(verbose_level, 2, f"Iteration {it+1} [Before updating]: |X_O - X_I| - Max difference: {numpy.nanmax(diff)}, Mean difference: {numpy.nanmean(diff)}")
 
         if eps_threshold is not None and numpy.all(diff[~numpy.isnan(diff)] < eps_threshold):
-            if verbose:
-                print(f"|X_0 - X_I| < {eps_threshold} - [eps_threshold flag reached] Optimization converged in {it} iterations.")
+            log_verbose(verbose_level, 1, f"[eps_threshold flag reached] : |X_0 - X_I| < {eps_threshold} - Optimization converged in {it} iterations.")
             break
-        
-        #===================================================
-        # Assembly the residual vector and Jacobian matrix
-        #===================================================
-        if verbose:
-            print("\n#=====================================================")
-            print(f"STARTING ITERATION {it+1} OF THE OPTIMIZATION PROCESS")
-            print("#=======================================================")
+
+        if eps_sum_threshold is not None and numpy.nansum(diff) < eps_sum_threshold:
+            log_verbose(verbose_level, 1, f"[eps_sum_threshold flag reached] : sum(|X_0 - X_I|) < {eps_sum_threshold} - Optimization converged in {it} iterations.")
+            break
 
         # Create masks to filter out invalid points
         mask_R = numpy.isfinite(R).all(axis=1)  # Create a mask for finite values in R
         mask_J = numpy.isfinite(J).all(axis=(1, 2))  # Create a mask for finite values in each row of J
         mask = mask_R & mask_J  # Combine the masks to filter out invalid points
-
-        if verbose:
-            print(f"Iteration {it+1}: {numpy.sum(mask)} valid points out of {Npoints}.")
+        log_verbose(verbose_level, 3, f"Iteration {it+1}: {numpy.sum(mask)} valid points out of {Npoints}.")
 
         # Apply the masks to R_flat and J_flat
         R = R[mask, :]  # shape (Nvalid_points, output_dim)
@@ -323,35 +340,17 @@ def optimize_parameters(
         JTR = numpy.dot(J_flat.T, R_flat)  # shape (Nparams,)
 
         # Check the gradient threshold
-        if verbose or gradient_threshold is not None:
+        if verbose_level >= 3 or gradient_threshold is not None:
             grad_norm = numpy.linalg.norm(JTR)  # shape ()
-
-        if verbose:
-            print(f"Iteration {it+1}: |J^T R| - Gradient norm: {grad_norm}")
-
+            log_verbose(verbose_level, 3, f"Iteration {it+1}: |J^T R| - Gradient norm: {grad_norm}")
+            
         if gradient_threshold is not None and grad_norm < gradient_threshold:
-            if verbose:
-                print(f"|J^T R| < {gradient_threshold} - [gradient_threshold flag reached] Optimization converged in {it} iterations.")
+            log_verbose(verbose_level, 1, f"[gradient_threshold flag reached] : |J^T R| < {gradient_threshold} - Optimization converged in {it} iterations.")
             break
-
-
-        #===================================================
-        # Regularization and conditioning part
-        #===================================================
-        make_reg = reg_factor is not None or precond_jacobi is True
-
-        # Display the condition number of the Jacobian matrix without regularization
-        if verbose and make_reg:
-            print(f"Iteration {it+1}: Condition number of JTJ before preconditionning and regularization: {numpy.linalg.cond(JTJ)}")
-            eigvals, eigvecs = numpy.linalg.eig(JTJ)
-            print(f"Iteration {it+1}: Eigenvalues of JTJ before preconditionning and regularization:\n{eigvals}")
 
         # Add regularization if requested
         if reg_factor is not None:
             JTJ += reg_factor * numpy.eye(transform.Nparams, dtype=Package.get_float_dtype())
-
-            if verbose:
-                print(f"Iteration {it+1}: Condition number of JTJ after regularization: {numpy.linalg.cond(JTJ)}")
 
         # Apply preconditioning if requested
         if precond_jacobi:
@@ -359,57 +358,50 @@ def optimize_parameters(
             diag_JTJ = numpy.diag(JTJ)
             
             if numpy.any(diag_JTJ == 0):
-                raise ValueError("Jacobi preconditioner cannot be applied because the diagonal of JTJ contains zeros.")
+                raise ValueError("Jacobi preconditioner cannot be applied because the diagonal of (J^T J) contains zeros.")
 
             # Apply the Jacobi preconditioner
             JTJ = JTJ / diag_JTJ[:, numpy.newaxis]  # Normalize each row by the diagonal element
             JTR = JTR / diag_JTJ  # Normalize the residual vector by the diagonal elements
 
-            if verbose:
-                print(f"Iteration {it+1}: Condition number of JTJ after Jacobi preconditioning: {numpy.linalg.cond(JTJ)}")
-
-        # ===================================================
         # Condition number check
-        # ===================================================
-
-        # Condition number check
-        if verbose or cond_cutoff is not None:
+        if verbose_level >= 3 or cond_cutoff is not None:
             cond_number = numpy.linalg.cond(JTJ)  # shape ()
-
-        if verbose:
-            print(f"Iteration {it+1}: Condition number of JTJ: {cond_number}")
-
-        if verbose:
-            eigvals, eigvecs = numpy.linalg.eig(JTJ)
-            print(f"Iteration {it+1}: Eigenvalues of JTJ:\n{eigvals}")
+            log_verbose(verbose_level, 3, f"Iteration {it+1}: Condition number of (J^T J): {cond_number}")
+            log_verbose(verbose_level, 3, f"Iteration {it+1}: Eigenvalues of (J^T J):\n{numpy.linalg.eigvals(JTJ)}")
 
         if cond_cutoff is not None and cond_number > cond_cutoff:
-            print(f"Warning: Condition number {cond_number} exceeds cutoff {cond_cutoff}. Optimization may be unstable. skipping iteration {it+1} and returning NaN array.")
+            log_verbose(verbose_level, 1, f"[cond_number flag reached] : Warning Condition number {cond_number} exceeds cutoff {cond_cutoff}. Optimization may be unstable. skipping iteration {it+1} and returning NaN array.")
             return numpy.full(transform.Nparams, numpy.nan, dtype=Package.get_float_dtype())
-
-        # ====================================================
-        # Solve the linear system to find the delta
-        # ====================================================
 
         # Solve the linear system to find the delta
         delta_itk = numpy.linalg.solve(JTJ, JTR) # shape (Nparams,)
+        log_verbose(verbose_level, 2, f"Iteration {it+1}: Solved delta parameters:\n{delta_itk}")
 
-        if verbose:
-            print(f"Iteration {it+1}: Delta parameters:\n{delta_itk}")
-            
         # Update the parameters of the object class
         object_class.parameters = object_class.parameters + delta_itk  # shape (Nparams,)
+        log_verbose(verbose_level, 2, f"Iteration {it+1}: Updated parameters:\n{object_class.parameters}")
 
-        if verbose:
-            print(f"Iteration {it+1}: Updated parameters:\n{object_class.parameters}")
+        # If full verbose is True, compute the new residuals after updating the parameters
+        if verbose_level >= 3:
+            transformed_points_itk, _, jacobian_dp = object_class._transform(input_points, dx=False, dp=True)  # shape (Npoints, output_dim), None, (Npoints, output_dim, Nparams)
+            R_precomputed = output_points - transformed_points_itk  # shape (Npoints, output_dim)
+            J_precomputed = jacobian_dp  # shape (Npoints, output_dim, Nparams)
+            log_verbose(verbose_level, 3, f"Iteration {it+1} [After updating]: |X_O - X_I| - Max difference: {numpy.nanmax(R_precomputed)}, Mean difference: {numpy.nanmean(R_precomputed)}")
 
         # Check the delta_p_threshold
         if delta_p_threshold is not None:
             delta_norm = numpy.linalg.norm(delta_itk)  # shape ()
 
             if delta_norm < delta_p_threshold:
-                if verbose:
-                    print(f"|delta_p| < {delta_p_threshold} - [delta_p_threshold flag reached] Optimization converged in {it+1} iterations.")
+                log_verbose(verbose_level, 1, f"[delta_p_threshold flag reached] : |delta_p| < {delta_p_threshold} - Optimization converged in {it+1} iterations.")
+                break
+
+        # Check the max_time
+        if max_time is not None:
+            elapsed_time = time.perf_counter() - starting_time  # shape ()
+            if elapsed_time > max_time:
+                log_verbose(verbose_level, 1, f"[max_time flag reached] : Warning Elapsed time {elapsed_time} seconds exceeds max_time {max_time} seconds - Optimization stopped at iteration {it+1}.")
                 break
     
     return object_class.parameters  # shape (Nparams,)
